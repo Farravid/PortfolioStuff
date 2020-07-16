cf4e4931472a775257ecba557850f2e8eceba959
Deivid <videdavid@hotmail.com>
2020-07-16 03:56:55 +0200

    Empezando playfab con photon y el creando proyecto Lost Ember, el cambio entre animales funciona, toca ponerse con la vuelta a humano, idea transfroms

---
+++ LostEmberSimulate/Assets/Scripts/CambioAnimal.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CambioAnimal : MonoBehaviour
+{
+    [SerializeField] private float distanciaCambio;
+    [SerializeField] private CinemachineFreeLook camaraAnimal;
+
+    private GameObject animalElegido;
+
+    private void Start()
+    {
+        camaraAnimal.Priority = 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        BuscarAnimal();     
+    }
+
+    private void BuscarAnimal()
+    {
+        float distanciaMenor = Mathf.Infinity;
+
+
+        GameObject[] posiblesAnimales = GameObject.FindGameObjectsWithTag("Cambiable");
+        if (posiblesAnimales != null)
+        {
+            foreach(GameObject animal in posiblesAnimales)
+            {
+                float dis = Vector3.Distance(this.transform.position, animal.transform.position);
+                Debug.Log("Distancia: " + dis);
+                //Debug.Log("Distancia entre yo y " + animal.gameObject.name + " es " + dis);
+                //El nombre del animal tiene que ser diferente al nuestro para que la distancia no sea 0 
+                if (dis <= distanciaCambio)
+                {
+                    if (animal.gameObject.name != this.gameObject.name)
+                    {
+                        Debug.Log("entro aqui que pasa");
+                        if (dis <= distanciaMenor)
+                        {
+                            Debug.Log("Aqui entro y el animal pasa a no ser null pq no");
+
+                            animalElegido = animal;
+                            distanciaMenor = dis;
+                        }
+                    }
+                }
+            }
+
+
+
+            //Si existe animal al que intercambiarse
+            if (animalElegido != null)
+            {
+                //Deberemos mostrar un hud
+                Debug.Log("Permitido el campo");
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    ///Todo esto pasarlo a un metodo lo que hay dentro de cuadno pulsamos E
+
+
+                    camaraAnimal.Priority = -1;
+                    //Aqui falta lo de ir cambiando de camaras de script y demas y tal pascualo
+                    //tambien deveremos hacer desaparecer el personaje
+
+
+
+                    //Si el objeto que controlamos es el jugador lo ponemos Active false, si es un animal deberemos activar otra vez el movimiento aleatorio
+                    if(this.gameObject.tag == "Player")
+                    {
+                        this.gameObject.SetActive(false);
+                        MonoBehaviour[] componentes = animalElegido.gameObject.GetComponents<MonoBehaviour>();
+                        foreach (MonoBehaviour c in componentes)
+                        {
+                            c.enabled = true;
+                        }
+                    }
+                    else {
+                        //Si pasamos de un animal a otro debemos hacer varias cosas
+
+                        //Y del animal que estamos manejando debemos desactivar el controller y activar el path random automatico
+                        MonoBehaviour[] componentesAnimalPropio = this.gameObject.GetComponents<MonoBehaviour>();
+                        foreach (MonoBehaviour c in componentesAnimalPropio)
+                        {
+                            c.enabled = false;
+                        }
+
+                        this.gameObject.GetComponent<Animator>().enabled = true;
+                        this.gameObject.GetComponent<Collider>().enabled = true;
+                        this.gameObject.GetComponent<CharacterController>().enabled = true;
+
+
+
+                        //Establecemos todos como activos del animal que vamos a pasar 
+                        MonoBehaviour[] componentesAnimalCambio = animalElegido.gameObject.GetComponents<MonoBehaviour>();
+                        foreach (MonoBehaviour c in componentesAnimalCambio)
+                        {
+                            c.enabled = true;
+                        }
+                    }
+                }
+            }
+
+            
+
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, distanciaCambio);
+    }
+
+}
---
+++ LostEmberSimulate/Assets/Scripts/Controllers/HumanController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class HumanController : MonoBehaviour
+{
+    #region Variables
+    //Entradas
+    private float InputX;
+    private float InputZ;
+
+    [Header("Velocidades y fuerzas para la fisica del personaje")]
+    public float moveSpeed = 3f;
+
+    [Header("Comprobaciones fisicas para el personaje")]
+    public float gravity = -9.81f;
+    private bool isGrounded;
+    //Vector que utilizaremos para establecer la gravedad del personaje
+    private Vector3 velocityGravity;
+
+    [Header("Smoothness Zooms y Rotaciones")]
+    [Range(0, 10)] public float rotateSpeed = 5f;
+
+    //Time.deltatime
+    private float delta;
+
+    //Charactaer controller
+    private CharacterController _cc;
+
+    //Vector al cual vamos a querer que se mueva nuestro personaje
+    private Vector3 desiredMovement;
+
+    //Minimo para las rotaciones cuando nos movemos
+    private float minimoRotacion = 0.1f;
+
+    //Velocidad de nuestros inputs para saber si estamso en movimiento o no
+    private float speedInputs;
+
+    //Animator
+    private Animator _animator;
+
+    //Referencia a la camara de cada controller para iniciarla cuando se inicie el script
+    [SerializeField] private CinemachineFreeLook camaraAnimalPropia;
+
+    #endregion
+
+
+    void Start()
+    {
+        camaraAnimalPropia.Priority = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        _cc = GetComponent<CharacterController>();
+        _animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        delta = Time.deltaTime;
+        MagnitudInput();
+        SetGravityGround();
+    }
+
+    #region PlayerMovementAndGravity
+
+    public void MagnitudInput()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        speedInputs = new Vector2(InputX, InputZ).sqrMagnitude;
+        if (speedInputs > minimoRotacion)
+        {
+            PlayerInput(true);
+        }
+        else
+        {
+            PlayerInput(false);
+        }
+    }
+    public void PlayerInput(bool rot)
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        //Hacemos este vector3 para que la velocidad diagonal como la estandar no se multipliquen y por lo tanto la velocidad maxima sea 1 en cualquier direccion
+        Vector3 playerInput;
+        playerInput = Vector3.ClampMagnitude(new Vector3(InputX, 0f, InputZ), 1);
+
+        Vector3 camForw = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+
+        camForw.y = 0f;
+        camRight.y = 0f;
+
+        camForw.Normalize();
+        camRight.Normalize();
+
+        desiredMovement = playerInput.x * camRight + playerInput.z * camForw;
+        desiredMovement = desiredMovement * moveSpeed;
+
+        _animator.SetFloat("velocidadPlayer", desiredMovement.sqrMagnitude);
+        
+
+        //Si rot es true se rotara si no no, es para controlar la animacion
+        if (rot)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMovement), rotateSpeed * delta);
+
+        _cc.Move(desiredMovement * delta);
+
+    }
+    public void SetGravityGround()
+    {
+        isGrounded = Physics.Raycast(_cc.bounds.min, Vector3.down, 0.1f);
+
+
+        if (isGrounded && velocityGravity.y < 0)
+        {
+            velocityGravity.y = 0f;
+        }
+
+        velocityGravity.y += gravity * Time.deltaTime;
+        //La aceleracion hay que multiplicar el tiempo al cuadrado;
+        _cc.Move(velocityGravity * Time.deltaTime);
+
+    }
+
+
+    #endregion
+
+}
